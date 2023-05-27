ea261cc41d37ebd5c7b77ed611cda68ce5817666
ezekielswanson <swansonezekiel@gmail.com>
2023-05-26 21:30:58 -0500

    Updated

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // javascript
+
+
+//log input of 
+//get the input 
+//get value on input 
+//run click event on publish 
+
+
+const endoresemntInput = document.getElementById('endorsement-input');
+const fromInput = document.getElementById('from-name');
+const toINput = document.getElementById('to-name');
+const sumbitBtn = document.getElementById('submit-btn');
+
+
+sumbitBtn.addEventListener('click', function() {
+    let endorseVal = endoresemntInput.value;
+    
+})
