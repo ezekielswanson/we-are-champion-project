a713f4990211ed4605dde4ac4a371c4f37d3b5f5
ezekielswanson <swansonezekiel@gmail.com>
2023-05-29 20:19:22 -0500

    Updated js

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-// javascript
 
 
-//log input of 
-//get the input 
-//get value on input 
-//run click event on publish 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://we-are-the-champions-app-default-rtdb.firebaseio.com/"
+}
+
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const endoresementlistInDB = ref(database, "endorsementList")
+
 
 
 const endoresemntInput = document.getElementById('endorsement-input');
 const fromInput = document.getElementById('from-name');
-const toINput = document.getElementById('to-name');
+const toInput = document.getElementById('to-name');
 const sumbitBtn = document.getElementById('submit-btn');
 
 
 sumbitBtn.addEventListener('click', function() {
     let endorseVal = endoresemntInput.value;
+    let fromVal = fromInput.value;
+    let toVal =  toInput.value;
+
+    let dataToPush = {
+        endorsement: 'endorseVal',
+        from: 'fromVal',
+        to: 'toVal'
+    }
+
+    push(endoresementlistInDB, dataToPush)
+    console.log(endorseVal)
     
 })
